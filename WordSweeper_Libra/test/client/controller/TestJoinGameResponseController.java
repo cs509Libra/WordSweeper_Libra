f26333d544faca118aeda90477cf77459d677383
Sicy <Sicy@LAPTOP-S6UVETJ3>
2016-12-10 14:48:43 -0500

    join game response controller finished

---
+++ WordSweeper_Libra/test/client/controller/TestJoinGameResponseController.java
@@ -0,0 +1,58 @@
+package client.controller;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import client.MockServerAccess;
+import client.controller.responseController.JoinGameResponseController;
+import client.model.Model;
+import client.view.Application;
+import client.view.MultiGame;
+import xml.Message;
+
+public class TestJoinGameResponseController {
+	/**@author You Zhou, Qingquan Zhao, Han Bao, Ruochen Shi (Authors contribute equally)
+	 * This is responsible for testing the controller of "Join Game Response" controller */
+		Model model = new Model();
+		Application client = new Application(model);
+		MockServerAccess mockServer = new MockServerAccess("localhost");
+
+		@Before
+		public void set() {
+			/**this is the preparation before the test.*/
+			// FIRST thing to do is register the protocol being used.
+			if (!Message.configure("wordsweeper.xsd")) {
+				fail("unable to configure protocol");
+			}
+			client.setVisible(true);
+			client.setServerAccess(mockServer);
+			
+			create_init_MultiGame();
+		}
+
+		public void create_init_MultiGame() {
+			String name="player1";
+			model.updateInfo("something different", "I don't know", "haha", 6, 7, "A,B,C,D,F,E,E,G,J,I,J,O,P,B,I,M", 55,
+					"6,6");
+			model.getPlayer().setName(name);
+			client.setMg(new MultiGame(model, client));
+		}
+		
+		@Test
+		public void TestJoinGameResponseProcess()
+		{
+			/**this is the test for the process of "Join Game Response" controller*/
+			String gameid="Game1";
+			model.getGame().setGameID(gameid);
+			String xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?><response id=\"someMessageID\" success=\"true\">"
+			           + "<joinGameResponse gameId=\"%s\"/></response>";
+			xml=String.format(xml,gameid);
+			Message m = new Message(xml);
+			JoinGameResponseController jgrc=new JoinGameResponseController(client,model);
+			assertTrue(jgrc.process(m));
+		}
+}
+//end of TestJoinGameResponseController
