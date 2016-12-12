8daecbb4af4fc684b2576b79eff58182588f57eb
Q-Zhao <qzhao@wpi.edu>
2016-12-12 18:45:29 -0500

    WindowListener added. If a player close window directly instead of click quit, an exit game message will still be sent to server

--- WordSweeper_Libra/src/client/view/MultiGame.java
+++ WordSweeper_Libra/src/client/view/MultiGame.java
@@ -1,9 +1,13 @@
 package client.view;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import client.controller.requestController.ExitGameController;
 import client.model.Model;
 
 /**
@@ -37,9 +41,10 @@ public class MultiGame extends JFrame {
 	 */
 	private void initiate(){		
 		setTitle("Wordsweeper");		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 520);
 		setAlwaysOnTop(false);
+		
+		this.addWindowListener(new CloseNotifier(app, model));
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -59,3 +64,30 @@ public class MultiGame extends JFrame {
 	}
 	
 }
+
+
+/**
+ * The CloseNotifier is a class extends WindowAdapter.
+ * In the case that the player simply close the window, a message will still be sent to the server to remove this player.
+ * @author You Zhou, Qingquan Zhao, Han Bao, Ruochen Shi (Authors contribute equally)
+ *
+ */
+
+class CloseNotifier extends WindowAdapter{
+	
+	Application app;
+	Model model;
+	
+	public CloseNotifier(Application app, Model model) {
+		this.app = app;
+		this.model = model;
+	}
+	/**
+	 * Call the exit game controller to send a exit game message to server.
+	 */
+	public void windowClosing(WindowEvent event){
+		new ExitGameController(app, model).process();
+		System.exit(0);
+	}
+}
+
